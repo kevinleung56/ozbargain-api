65a0603d9ec19faccdc3e1165d1270dd430a05c5
Kevin Leung <kevinleung56@gmail.com>
2020-12-07 14:11:36 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// eslint-disable-next-line import/no-unresolved
+const express = require("express");
+
+const app = express();
+
+// Routes
+app.get("/*", (req, res) => {
+  res.send(`Request received: ${req.method} - ${req.path}`);
+});
+
+// Error handler
+app.use((err, req, res) => {
+  console.error(err);
+  res.status(500).send("Internal Serverless Error");
+});
+
+module.exports = app;
