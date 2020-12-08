7adc5be44b4d154cd960e66ffbf5ad925f26509d
Kevin Leung <kevinleung56@gmail.com>
2020-12-08 16:22:23 +1100

    Add RSS parser library + set up deals endpoint

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 
 // eslint-disable-next-line import/no-unresolved
 const express = require("express");
+const parser = require("./parser");
 
 const app = express();
 
 // Routes
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.send(`Request received: ${req.method} - ${req.path}`);
+});
+
+app.get("/deals/", async (req, res) => {
+  let deals = await parser.fetchDeals();
+  res.send(
+    `Here are the deals you ordered (amount = ${
+      deals.length
+    }): ${deals.map((i) => JSON.stringify(i))}`
+  );
 });
 
 // Error handler
