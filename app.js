31b201a06ab4d6ca971927f1054ce0b28a235e16
Kevin Leung <kevinleung56@gmail.com>
2020-12-12 21:03:45 +1100

    Utilize x-ray library

--- app.js
+++ app.js
@@ -12,24 +12,21 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/deals', async (req, res) => {
-  try {
-    const deals = await scraper.fetchDeals();
-    res.status(200).json({
-      success: true,
-      errorCode: '',
-      errorDescription: '',
-      deals: deals,
-    });
-  } catch {
+  const deals = await scraper.fetchDeals();
+  if (deals) {
+    res.status(200).json(deals);
+  } else {
     console.log(`Something is wrong with deals object: ${deals}`);
     res.status(503).send('Something went wrong. Try again later');
   }
 });
 
 // Error handler
-app.use((err, req, res) => {
-  console.error(err);
-  res.status(500).send('Internal Serverless Error');
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error(err);
+    res.status(500).send('Internal Serverless Error');
+  }
 });
 
 // DEBUG
--- scraper.js
+++ scraper.js
@@ -1,44 +1,44 @@
 'use strict';
 
+const xray = require('x-ray');
 const cheerio = require('cheerio');
 const axios = require('axios').default;
+
+const x = new xray();
 const OZBARGAIN_FEED_URL = 'https://www.ozbargain.com.au/deals';
 
-async function fetchDeals() {
-  return await axios.get(OZBARGAIN_FEED_URL).then(function (response) {
-    const $ = cheerio.load(response.data);
-    let deals = [];
-    $('.node-ozbdeal').map((i, el) => {
-      const dealExpired = $(el).find($('span.expired')).text();
-      if (
-        !dealExpired.includes('expired') &&
-        !dealExpired.includes('out of stock')
-      ) {
-        const id = $(el).find($('h2.title')).find('a').attr('href').substr(6);
-        const title = $(el).find($('h2.title')).find('a').text();
-        const upvotes = $(el).find($('span.voteup')).children().last().text();
-        const downvotes = $(el)
-          .find($('span.votedown'))
-          .children()
-          .last()
-          .text();
-        const time_posted = $(el)
-          .find($('div.submitted'))
-          .contents()
-          .filter((i, child) => child.type === 'text')
-          .text()
-          .substring(4, 22);
-        const deal = {
-          id: id,
-          title: title,
-          upvotes: upvotes,
-          downvotes: downvotes,
-          time_posted: time_posted,
-        };
-        deals.push(deal);
-      }
-    });
-    return deals;
+function fetchDeals() {
+  return new Promise(function (resolve, reject) {
+    x(OZBARGAIN_FEED_URL, '.node-ozbdeal', [
+      {
+        title: 'h2.title@data-title',
+        link: 'h2.title a@href',
+        meta: {
+          submitted: 'div.submitted',
+          image: 'img.gravatar@src',
+        },
+        content: 'div.content@html',
+        vote: {
+          up: 'span.voteup',
+          down: 'span.votedown',
+        },
+        gravatar: 'img.gravatar@src',
+
+        snapshot: {
+          link: '.foxshot-container a@href',
+          title: '.foxshot-container a@title',
+          image: '.foxshot-container img@src',
+        },
+        category: 'ul.links span.tag a',
+      },
+    ])
+      .paginate('a.pager-next@href')
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function (e) {
+        reject(e);
+      });
   });
 }
 
